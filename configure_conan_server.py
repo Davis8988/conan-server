d36a5f7501e0c09e7adf5ab6030fbaf56e884bc9
= <=>
2021-01-05 11:22:47 +0200

    Added 'export' command to vars set && Added check for envs in python script and also check for conan-server config ini file: '{conan_server_config_file}'

---
+++ configure_conan_server.py
@@ -0,0 +1,26 @@
+
+# This script configures the conan-server file: 'server.conf'
+#   It's default location should be: '/root/.conan_server/server.conf'
+
+import os
+import sys
+import configparser
+
+conan_server_config_file_default = '/root/.conan_server/server.conf'
+conan_server_config_file = os.environ.get("CONAN_SERVER_CONFIG_FILE", conan_server_config_file_default)
+
+print(f"Configuring conan-server config ini file: '{conan_server_config_file}' ")
+if not os.path.exists(conan_server_config_file):
+	print(f"Error - Missing conan-server config ini file: '{conan_server_config_file}' \n cannot configure conan-server")
+	sys.exit(1)
+	
+
+config = configparser.ConfigParser()
+
+try:
+	print(f"Reading config ini file: '{conan_server_config_file}' ")
+	config.read(conan_server_config_file)
+except BaseException as error_msg:
+	print(f"Error - Failed reading conan-server config ini file: '{conan_server_config_file}'\n{error_msg}")
+	sys.exit(1)
+
