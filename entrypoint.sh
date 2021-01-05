91bd498c7c05ec31fd7042248417127142e2b45e
= <=>
2021-01-05 10:48:07 +0200

    Added printings to entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Run server on port 9300 with 4 workers and a timeout of 5 minutes (300 seconds)
-#gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app
-
-echo yes
-
+echo python Version:
 python --version
+echo Configuring conan-server ini file: '/root/.conan_server/server.conf'
+echo Executing: python $(pwd)/configure_conan_server.py
+python $(pwd)/configure_conan_server.py
 
-echo Executing python script:
-python python-pre-start-script.py
+
+echo Finished configuring conan-server ini file: '/root/.conan_server/server.conf'
+echo Starting conan-server:
+echo Executing: gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app
+
+# Run server on port 9300 with 4 workers and a timeout of 5 minutes (300 seconds)
+gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app
+
+echo Finished executing conan-server
 
