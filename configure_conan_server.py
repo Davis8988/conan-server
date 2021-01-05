d6f2f2bb2077769e3959de69d4d491a7585a24e2
= <=>
2021-01-05 13:45:32 +0200

    Added check for: conan_server_config_file not null

--- configure_conan_server.py
+++ configure_conan_server.py
@@ -13,6 +13,11 @@ conan_server_read_permissions = os.environ.get("CONAN_SERVER_READ_PERMISSIONS", 
 conan_server_write_permissions = os.environ.get("CONAN_SERVER_WRITE_PERMISSIONS", None)
 
 
+if not conan_server_config_file:
+	print(f"Error - Path to conan-server config ini file is null: '{conan_server_config_file}'\nDid you set env var: 'CONAN_SERVER_CONFIG_FILE' to it?\n cannot configure conan-server")
+	sys.exit(1)
+
+
 # Check conan-server config file exists
 print(f"Configuring conan-server config ini file: '{conan_server_config_file}' ")
 if not os.path.exists(conan_server_config_file):
@@ -24,9 +29,9 @@ if not os.path.exists(conan_server_config_file):
 if conan_server_creds_list:
 	print("Preparing creds list to configure")
 	conan_server_creds_list = conan_server_creds_list.split(";")
-if type(conan_server_creds_list) != list:
-	print(f"Error - Failed to convert var 'conan_server_creds_list={conan_server_creds_list}' to type 'list' using split:  conan_server_creds_list=conan_server_creds_list.split(';')\ntype(conan_server_creds_list)=={type(conan_server_creds_list)}")
-	sys.exit(1)
+	if type(conan_server_creds_list) != list:
+		print(f"Error - Failed to convert var 'conan_server_creds_list={conan_server_creds_list}' to type 'list' using split:  conan_server_creds_list=conan_server_creds_list.split(';')\ntype(conan_server_creds_list)=={type(conan_server_creds_list)}")
+		sys.exit(1)
 
 
 # Read conan-server config file
@@ -37,4 +42,5 @@ try:
 except BaseException as error_msg:
 	print(f"Error - Failed reading conan-server config ini file: '{conan_server_config_file}'\n{error_msg}")
 	sys.exit(1)
+
 
