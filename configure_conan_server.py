3066942ab0c590b2f522d7f148876188d272f6ce
= <=>
2021-01-05 11:38:34 +0200

    Docs

--- configure_conan_server.py
+++ configure_conan_server.py
@@ -13,15 +13,16 @@ conan_server_user_pass = os.environ.get("CONAN_SERVER_USER_PASS", None)
 conan_server_read_permissions = os.environ.get("CONAN_SERVER_READ_PERMISSIONS", None)
 
 
+# Check conan-server config file exists
 print(f"Configuring conan-server config ini file: '{conan_server_config_file}' ")
 if not os.path.exists(conan_server_config_file):
 	print(f"Error - Missing conan-server config ini file: '{conan_server_config_file}' \n cannot configure conan-server")
 	sys.exit(1)
 	
 
-config = configparser.ConfigParser()
-
+# Read conan-server config file
 try:
+	config = configparser.ConfigParser()
 	print(f"Reading config ini file: '{conan_server_config_file}' ")
 	config.read(conan_server_config_file)
 except BaseException as error_msg:
