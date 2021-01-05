63989be09b5c3382eaaee5918fb6456fa2404188
= <=>
2021-01-05 11:56:21 +0200

    Changed env name to: CONAN_SERVER_CREDS_LIST

--- configure_conan_server.py
+++ configure_conan_server.py
@@ -8,8 +8,7 @@ import configparser
 
 
 conan_server_config_file = os.environ.get("CONAN_SERVER_CONFIG_FILE", None)
-conan_server_user_name = os.environ.get("CONAN_SERVER_USER_NAME", None)
-conan_server_user_pass = os.environ.get("CONAN_SERVER_USER_PASS", None)
+conan_server_creds_list = os.environ.get("CONAN_SERVER_CREDS_LIST", None)
 conan_server_read_permissions = os.environ.get("CONAN_SERVER_READ_PERMISSIONS", None)
 conan_server_write_permissions = os.environ.get("CONAN_SERVER_WRITE_PERMISSIONS", None)
 
