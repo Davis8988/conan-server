fab141ec705b116785e9c90650ff0472aae0db92
= <=>
2021-01-05 11:02:06 +0200

    Docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ CONAN_SERVER_TIMEOUT_SECONDS=${CONAN_SERVER_TIMEOUT_SECONDS:-5}
 CONAN_SERVER_USER_NAME=${CONAN_SERVER_USER_NAME:-admin}
 CONAN_SERVER_USER_PASSWORD=${CONAN_SERVER_USER_PASSWORD:-12345678}
 
-
+# Print Python version
 echo python Version:
 python --version
 
@@ -20,7 +20,7 @@ if [ "$?" != "0" ]; then echo Error - Failed configuring conan-server ini file: 
 echo Finished configuring conan-server ini file: '/root/.conan_server/server.conf'
 echo ""
 
-# Start conan-server
+# Print Confs
 echo Conan-Server Vars:
 echo   Listen Address: $CONAN_SERVER_LISTEN_IP:$CONAN_SERVER_LISTEN_PORT
 echo   Workers: $CONAN_SERVER_WORKERS_COUNT
@@ -28,6 +28,7 @@ echo   Timeout Seconds: $CONAN_SERVER_TIMEOUT_SECONDS
 echo   User: $CONAN_SERVER_USER_NAME
 echo ""
 
+# Start conan-server
 echo Starting conan-server:
 echo Executing: gunicorn -b ${CONAN_SERVER_LISTEN_IP}:${CONAN_SERVER_LISTEN_PORT} -w ${CONAN_SERVER_WORKERS_COUNT} -t ${CONAN_SERVER_TIMEOUT_SECONDS} conans.server.server_launcher:app
 # Run server on port ${CONAN_SERVER_LISTEN_PORT} with ${CONAN_SERVER_WORKERS_COUNT} workers and a timeout of ${CONAN_SERVER_TIMEOUT_SECONDS} seconds
